3b76ea1090b5093401c4136cfb69146ad2f1137d
huzekang <1040080742@qq.com>
2021-07-02 10:09:39 +0800

    1. 优化注释

--- usql-core/src/main/scala/com/chinaunicom/usql/core/config/package.scala
+++ usql-core/src/main/scala/com/chinaunicom/usql/core/config/package.scala
@@ -4,6 +4,9 @@ package com.chinaunicom.usql.core
 import com.chinaunicom.usql.core.config.Settings.Key
 
 package object config {
+  /**
+   * 用于stream query的调试，如果开启，则结构化流在读取到数据后就结束，而不会一直监听数据来源。
+   */
   val DebugEnable: Key[Boolean] = Key("stream.debug").boolean
   val HiveEnable: Key[Boolean] = Key("stream.hive.enable").boolean
   val JobTemplateEnable: Key[Boolean] = Key("stream.template.enable").boolean
